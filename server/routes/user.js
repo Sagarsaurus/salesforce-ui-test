3348f1e3364b20b080f4f59d47e2132fb00a9142
sagar.laud@oracle.com <sagrsccr@gmail.com>
2018-06-24 21:24:38 -0400

    Fixed another typo

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,7 +8,7 @@ module.exports = (router) => {
     //create a user
     router
         .route('/user')
-        .post(multipartWare, userController.createStory)
+        .post(multipartWare, userController.createUser)
 
     //get specific user
     router
